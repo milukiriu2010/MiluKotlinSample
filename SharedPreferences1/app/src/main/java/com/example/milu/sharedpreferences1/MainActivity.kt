2d8db652153c28b4eb1fb936a299ee38b68b557a
Mitsuharu Kiriu <milu.kiriu2010@gmail.com>
2018-06-07 20:34:32 +0900

    string.xml

--- SharedPreferences1/app/src/main/java/com/example/milu/sharedpreferences1/MainActivity.kt
+++ SharedPreferences1/app/src/main/java/com/example/milu/sharedpreferences1/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.milu.sharedpreferences1
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Vibrator
+import android.os.VibrationEffect
+import android.os.VibrationEffect.DEFAULT_AMPLITUDE
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -21,6 +25,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            this.saveName()
+            this.displayName()
+        }
+
+        vibration.setOnClickListener {
+            val vibrator : Vibrator = getSystemService( Context.VIBRATOR_SERVICE ) as Vibrator
+            val vibrationEffect = VibrationEffect.createOneShot( 1000, DEFAULT_AMPLITUDE )
+            vibrator.vibrate( vibrationEffect )
+            //vibrator.vibrate(300)
+        }
     }
 
     fun saveName() {
@@ -32,12 +48,21 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        val sp: SharedPreferences = this.getSharedPreferences(com.example.milu.sharedpreferences1.MainActivity.SHARED_PREF_NAME, com.example.milu.sharedpreferences1.MainActivity.KEY_NAME )
+        val sp: SharedPreferences = this.getSharedPreferences(com.example.milu.sharedpreferences1.MainActivity.SHARED_PREF_NAME, MODE_PRIVATE )
         val editor: SharedPreferences.Editor = sp.edit();
 
         editor.putString(KEY_NAME, name)
         editor.apply()
 
-        editText.text = ""
+        editText.setText( "".toCharArray(),  0, 0 )
+    }
+
+    fun displayName(){
+        val sp: SharedPreferences = this.getSharedPreferences(com.example.milu.sharedpreferences1.MainActivity.SHARED_PREF_NAME, MODE_PRIVATE )
+        val name = sp.getString(KEY_NAME, null )
+
+        if ( name != null ){
+            textView.text = "Welcome " + name
+        }
     }
 }
