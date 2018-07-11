76e865a612a4512ebafc0e83d12a2684bf9b82b6
milu(Mitsuharu Kiriu) <milu.kiriu2010@gmail.com>
2018-07-11 17:34:52 +0900

    AsncTask

--- RadioGroup12/app/src/main/java/com/example/milu/radiogroup1/MainActivity.kt
+++ RadioGroup12/app/src/main/java/com/example/milu/radiogroup1/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.milu.radiogroup1
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
+import com.example.milu.net.HttpGet
+import com.example.milu.xml.MyXMLParse
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +26,57 @@ class MainActivity : AppCompatActivity() {
                 val radio: RadioButton = findViewById(checkedId) as RadioButton
                 Toast.makeText(applicationContext, "On checked change : ${radio.text}", Toast.LENGTH_LONG).show()
             })
+
+        btnGet.setOnClickListener{
+            Log.d(this.javaClass.name,"XXXXXX")
+            val httpGetTask = HttpGetTask()
+            httpGetTask.execute(URL("https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2643123"))
+        }
+
+        btnDOM.setOnClickListener{
+            Log.d(this.javaClass.name, this.loadXMLbyDOM() )
+        }
     }
+
+    private fun loadXMLbyDOM(): String{
+        val myXmlParse = MyXMLParse()
+        val istream = this.resources.openRawResource(R.raw.myxml)
+        val br = BufferedReader(InputStreamReader(istream))
+
+        var line: String? = null
+        val sb = StringBuffer()
+        while ( { line = br.readLine(); line } != null ){
+            sb.append(line)
+        }
+
+        val xmlDoc = myXmlParse.str2doc(sb.toString())
+        val nodeListTitle = myXmlParse.searchNodeList( xmlDoc, "//PreferenceScreen/food/name")
+
+        val foodLst = mutableListOf<String>()
+        for ( i in 0 until nodeListTitle.length){
+            val nodeTitle = nodeListTitle.item(i)
+            val title = myXmlParse.searchNodeText( nodeTitle, "./text()")
+            foodLst.add(title)
+        }
+
+        return foodLst.joinToString(separator = "\n", prefix = "DOM\n")
+
+    }
+
+    class HttpGetTask: AsyncTask<URL, Unit, String>() {
+
+        override fun doInBackground(vararg params: URL?): String {
+            Log.d("b1", "XXXXXXX")
+            val httpGet = HttpGet()
+            val strGet = httpGet.doGet(params[0]!!)
+            Log.d("b1", strGet)
+            return strGet
+        }
+
+        override fun onPostExecute(result: String?) {
+            Log.d("b2",result)
+            super.onPostExecute(result)
+        }
+    }
+
 }
