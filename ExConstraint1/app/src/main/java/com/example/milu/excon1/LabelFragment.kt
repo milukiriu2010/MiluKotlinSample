c2b21462fef9b7f42801de7a614fb2a5c7d22544
Mitsuharu Kiriu <milu.kiriu2010@gmail.com>
2018-08-13 20:50:40 +0900

    fragment

---
+++ ExConstraint1/app/src/main/java/com/example/milu/excon1/LabelFragment.kt
@@ -0,0 +1,36 @@
+package com.example.milu.excon1
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.milu.intent2.R
+
+class LabelFragment: Fragment() {
+    private var counter = 0
+    private lateinit var counterLabel: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        counter = arguments?.getInt("counter") ?: 0
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.fragment_label, container, false )
+        counterLabel.setText(counter.toString())
+        return view
+    }
+}
+
+fun newLabelFragment(value: Int): LabelFragment {
+    val fragment = LabelFragment()
+
+    val args = Bundle()
+    args.putInt("counter",value)
+
+    fragment.arguments = args
+    return fragment
+}
