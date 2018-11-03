a5b17eb7a961637bb1eb7831a92a2e7dcefd0c06
milu.kiriu2010 <milu.kiriu2010@gmail.com>
2018-11-03 11:31:30 +0900

    accel

---
+++ ExConstraint2/app/src/main/java/milu/kiriu2010/excon2/screen1/sensorstep/StepDetector.kt
@@ -0,0 +1,74 @@
+package milu.kiriu2010.excon2.screen1.sensorstep
+
+class StepDetector {
+    private val ACCEL_RING_SIZE = 50
+    private val VEL_RING_SIZE = 10
+
+    // change this threshold according to your sensitivity preferences
+    private val STEP_THRESHOLD = 50f
+
+    // 250msec
+    private val STEP_DELAY_NS = 250000000
+
+    private var accelRingCounter = 0
+    private val accelRingX = FloatArray(ACCEL_RING_SIZE)
+    private val accelRingY = FloatArray(ACCEL_RING_SIZE)
+    private val accelRingZ = FloatArray(ACCEL_RING_SIZE)
+    private var velRingCounter = 0
+    private val velRing = FloatArray(VEL_RING_SIZE)
+    private var lastStepTimeNs: Long = 0
+    private var oldVelocityEstimate = 0f
+
+    private var listener: StepListener? = null
+
+    fun registerListener(listener: StepListener) {
+        this.listener = listener
+    }
+
+    fun updateAccel(timeNs: Long, x: Float, y: Float, z: Float) {
+        val currentAccel = FloatArray(3)
+        currentAccel[0] = x
+        currentAccel[1] = y
+        currentAccel[2] = z
+
+        // First step is to update our guess of where the global z vector is.
+        // 加速度センサの値を50個取得し、平均値をworldZに格納しているっぽい
+        accelRingCounter++
+        accelRingX[accelRingCounter % ACCEL_RING_SIZE] = currentAccel[0]
+        accelRingY[accelRingCounter % ACCEL_RING_SIZE] = currentAccel[1]
+        accelRingZ[accelRingCounter % ACCEL_RING_SIZE] = currentAccel[2]
+
+        val worldZ = FloatArray(3)
+        worldZ[0] = SensorFilter.sum(accelRingX) / Math.min(accelRingCounter, ACCEL_RING_SIZE)
+        worldZ[1] = SensorFilter.sum(accelRingY) / Math.min(accelRingCounter, ACCEL_RING_SIZE)
+        worldZ[2] = SensorFilter.sum(accelRingZ) / Math.min(accelRingCounter, ACCEL_RING_SIZE)
+
+        // worldZのベクタを取得.すなわち全体平均ベクタ値を取得
+        val normalization_factor = SensorFilter.norm(worldZ)
+
+        // worldZを正規化する
+        worldZ[0] = worldZ[0] / normalization_factor
+        worldZ[1] = worldZ[1] / normalization_factor
+        worldZ[2] = worldZ[2] / normalization_factor
+
+        // worldZと加速度センサの現在値を掛け合わせた後、worldZのベクタ値を引いている
+        // currentZは、"今回ベクタ値と全体平均ベクタ値の差"と思われる
+        val currentZ = SensorFilter.dot(worldZ, currentAccel) - normalization_factor
+        velRingCounter++
+        velRing[velRingCounter % VEL_RING_SIZE] = currentZ
+
+        // 速度の概算値？
+        val velocityEstimate = SensorFilter.sum(velRing)
+
+        // 次の場合、カウントアップ
+        // ・今回の計測がスレッシュホールド(50)を超えている
+        // ・前回の計測がスレッシュホールド(50)を超えていない
+        // ・今回の計測が前回から250msecたっている
+        if (velocityEstimate > STEP_THRESHOLD && oldVelocityEstimate <= STEP_THRESHOLD
+                && timeNs - lastStepTimeNs > STEP_DELAY_NS) {
+            listener!!.step(timeNs)
+            lastStepTimeNs = timeNs
+        }
+        oldVelocityEstimate = velocityEstimate
+    }
+}
---
+++ ExConstraint2/app/src/main/java/milu/kiriu2010/excon2/screen1/sensorstep/StepListener.kt
@@ -0,0 +1,5 @@
+package milu.kiriu2010.excon2.screen1.sensorstep
+
+interface StepListener {
+    fun step(timeNs: Long)
+}
