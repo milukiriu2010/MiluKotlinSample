82d01c88965fd32bc3553b7550f0cf59c1741020
Milu Japan <milu.kiriu2010@gmail.com>
2019-04-27 22:37:58 +0900

    w42

--- ExDB1/app/src/main/java/milu/kiriu2010/exdb1/opengl04/OpenGL04HomeFragment.kt
+++ ExDB1/app/src/main/java/milu/kiriu2010/exdb1/opengl04/OpenGL04HomeFragment.kt
@@ -31,18 +31,18 @@ class OpenGL04HomeFragment : Fragment() {
 
         myGLView = view.findViewById(R.id.myGL02ViewA04)
         val bmp0 = BitmapFactory.decodeResource(resources,R.drawable.texture_w42)
-        val render = W042Renderer()
+        val render = W042Renderer(context!!)
         render.bmpArray.add(bmp0)
         myGLView.setRenderer(render)
         myGLView.setOnTouchListener { v, event ->
             when (event.action) {
                 MotionEvent.ACTION_UP -> {
-                    render.rotateSwitch = false
+                    render.isRunning = false
                 }
                 MotionEvent.ACTION_DOWN -> {
                     Log.d(javaClass.simpleName,"ex[${event.x}]ey[${event.y}]")
                     Log.d(javaClass.simpleName,"vw[${myGLView.width}]vh[${myGLView.height}]")
-                    render.rotateSwitch = true
+                    render.isRunning = true
                     render.receiveTouch(event,myGLView.width,myGLView.height)
                 }
                 MotionEvent.ACTION_MOVE -> {
--- ExDB1/app/src/main/java/milu/kiriu2010/exdb1/opengl04/w042/W042Shader.kt
+++ ExDB1/app/src/main/java/milu/kiriu2010/exdb1/opengl04/w042/W042Shader.kt
@@ -2,8 +2,10 @@ package milu.kiriu2010.exdb1.opengl04.w042
 
 import android.opengl.GLES20
 import milu.kiriu2010.gui.basic.MyGLFunc
+import milu.kiriu2010.gui.model.MgModelAbs
+import milu.kiriu2010.gui.shader.MgShader
 
-class W042Shader {
+class W042Shader: MgShader() {
     // 頂点シェーダ
     private val scv =
             """
@@ -67,13 +69,94 @@ class W042Shader {
             }
             """.trimIndent()
 
-    fun loadShader(): Int {
+    override fun loadShader(): MgShader {
         // 頂点シェーダを生成
         val svhandle = MyGLFunc.loadShader(GLES20.GL_VERTEX_SHADER, scv)
         // フラグメントシェーダを生成
         val sfhandle = MyGLFunc.loadShader(GLES20.GL_FRAGMENT_SHADER, scf)
 
         // プログラムオブジェクトの生成とリンク
-        return MyGLFunc.createProgram(svhandle,sfhandle, arrayOf("a_Position","a_Normal","a_Color","a_TextureCoord") )
+        programHandle = MyGLFunc.createProgram(svhandle,sfhandle, arrayOf("a_Position","a_Normal","a_Color","a_TextureCoord") )
+        return this
+    }
+
+
+    fun draw(model: MgModelAbs,
+             matM: FloatArray,
+             matMVP: FloatArray,
+             matI: FloatArray,
+             u_vecLight: FloatArray,
+             u_vecEye: FloatArray,
+             u_Texture0: Int) {
+
+        GLES20.glUseProgram(programHandle)
+
+        // attribute(頂点)
+        model.bufPos.position(0)
+        GLES20.glGetAttribLocation(programHandle,"a_Position").also {
+            GLES20.glVertexAttribPointer(it,3,GLES20.GL_FLOAT,false, 3*4, model.bufPos)
+            GLES20.glEnableVertexAttribArray(it)
+        }
+        MyGLFunc.checkGlError("a_Position:${javaClass.simpleName}")
+
+        // attribute(法線)
+        model.bufNor.position(0)
+        GLES20.glGetAttribLocation(programHandle,"a_Normal").also {
+            GLES20.glVertexAttribPointer(it,3,GLES20.GL_FLOAT,false, 3*4, model.bufNor)
+            GLES20.glEnableVertexAttribArray(it)
+        }
+        MyGLFunc.checkGlError("a_Normal")
+
+        // attribute(色)
+        model.bufCol.position(0)
+        GLES20.glGetAttribLocation(programHandle,"a_Color").also {
+            GLES20.glVertexAttribPointer(it,4,GLES20.GL_FLOAT,false, 4*4, model.bufCol)
+            GLES20.glEnableVertexAttribArray(it)
+        }
+        MyGLFunc.checkGlError("a_Color")
+
+        // attribute(テクスチャコード)
+        model.bufTxc.position(0)
+        GLES20.glGetAttribLocation(programHandle,"a_TextureCoord").also {
+            GLES20.glVertexAttribPointer(it,2,GLES20.GL_FLOAT,false, 2*4, model.bufTxc)
+            GLES20.glEnableVertexAttribArray(it)
+        }
+        MyGLFunc.checkGlError("a_TextureCoord")
+
+        // uniform(モデル)
+        GLES20.glGetUniformLocation(programHandle,"u_matM").also {
+            GLES20.glUniformMatrix4fv(it,1,false,matM,0)
+        }
+        MyGLFunc.checkGlError("u_matM")
+
+        // uniform(モデル×ビュー×プロジェクション)
+        GLES20.glGetUniformLocation(programHandle,"u_matMVP").also {
+            GLES20.glUniformMatrix4fv(it,1,false,matMVP,0)
+        }
+        MyGLFunc.checkGlError("u_matMVP")
+
+        // uniform(逆行列)
+        GLES20.glGetUniformLocation(programHandle,"u_matINV").also {
+            GLES20.glUniformMatrix4fv(it,1,false,matI,0)
+        }
+
+        // uniform(ライト座標)
+        GLES20.glGetUniformLocation(programHandle,"u_vecLight").also {
+            GLES20.glUniform3fv(it,1,u_vecLight,0)
+        }
+
+        // uniform(視点座標)
+        GLES20.glGetUniformLocation(programHandle,"u_vecEye").also {
+            GLES20.glUniform3fv(it,1,u_vecEye,0)
+        }
+
+        // uniform(テクスチャ0)
+        GLES20.glGetUniformLocation(programHandle, "u_Texture0").also {
+            GLES20.glUniform1i(it, u_Texture0)
+        }
+        MyGLFunc.checkGlError("u_Texture0")
+
+        // モデルを描画
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, model.datIdx.size, GLES20.GL_UNSIGNED_SHORT, model.bufIdx)
     }
 }
