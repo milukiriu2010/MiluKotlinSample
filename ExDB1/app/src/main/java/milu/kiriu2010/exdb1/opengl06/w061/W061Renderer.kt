97e03519b73d4b0099fbacd35f82bba544f9f813
milu(Mitsuharu Kiriu) <milu.kiriu2010@gmail.com>
2019-05-07 08:40:03 +0900

    w61

--- ExDB1/app/src/main/java/milu/kiriu2010/exdb1/opengl06/w061/W061Renderer.kt
+++ ExDB1/app/src/main/java/milu/kiriu2010/exdb1/opengl06/w061/W061Renderer.kt
@@ -8,15 +8,14 @@ import android.os.SystemClock
 import android.util.Log
 import milu.kiriu2010.gui.basic.MyGLFunc
 import milu.kiriu2010.gui.basic.MyNoiseX
-import milu.kiriu2010.gui.color.MgColor
 import milu.kiriu2010.gui.model.Box01Model
-import milu.kiriu2010.gui.model.Cube01Model
 import milu.kiriu2010.gui.model.Particle01Model
 import milu.kiriu2010.gui.model.Torus01Model
 import milu.kiriu2010.gui.renderer.MgRenderer
 import java.nio.IntBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+import kotlin.random.Random
 
 // 被写界深度
 //   パーティクルフォグ
@@ -60,13 +59,47 @@ class W061Renderer(ctx: Context): MgRenderer(ctx) {
     // 深度値
     var u_depthCoef = 0.05f
 
-
-
     // テクスチャ座標変換行列
     private val matTex = FloatArray(16)
 
     // プロジェクションxビュー(正射影用の座標変換行列)
     private val matVP4T = FloatArray(16)
+
+    // パーティクル用のデータ
+    private val particleCount = 30
+    private val offsetPositionX = FloatArray(particleCount)
+    private val offsetPositionZ = FloatArray(particleCount)
+    private val offsetPositionS = FloatArray(particleCount)
+    private val offsetTexCoordS = FloatArray(particleCount)
+    private val offsetTexCoordT = FloatArray(particleCount)
+
+    init {
+        (0 until particleCount).forEach{ i ->
+            // Random.nextFloat() => 0 - 1
+            offsetPositionX[i] =  Random.nextFloat()*6f - 3f
+            offsetPositionZ[i] = -Random.nextFloat()*1.5f + 0.5f
+            offsetPositionS[i] =  Random.nextFloat()*0.02f
+            offsetTexCoordS[i] =  Random.nextFloat()
+            offsetTexCoordT[i] =  Random.nextFloat()
+        }
+        offsetPositionZ.sort()
+        Log.d(javaClass.simpleName,"create noise bitmap start")
+
+        // ノイズを生成するビットマップに描く
+        val noise = MyNoiseX(5,2,0.6f)
+        noise.seed = (SystemClock.uptimeMillis()/1000).toInt()
+        val size = 128
+        val noiseColor = FloatArray(size*size)
+        (0 until size).forEach { i ->
+            (0 until size).forEach { j ->
+                noiseColor[i*size+j] = noise.snoise(i.toFloat(),j.toFloat(),size.toFloat())
+            }
+        }
+        val bmp = noise.createImage(size,noiseColor)
+        bmpArray.add(0,bmp)
+
+        Log.d(javaClass.simpleName,"create noise bitmap end")
+    }
 
     override fun onDrawFrame(gl: GL10?) {
         angle[0] =(angle[0]+1)%360
@@ -178,13 +211,22 @@ class W061Renderer(ctx: Context): MgRenderer(ctx) {
         // テクスチャのバインド
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,frameTexture[0])
-        //GLES20.glActiveTexture(GLES20.GL_TEXTURE1)
-        //GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,texture[0])
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE1)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,textures[0])
 
         // -------------------------------------------------------
         // パーティクルをレンダリング
         // -------------------------------------------------------
+        (0 until particleCount).forEach { i ->
+            offsetPositionX[i] += offsetPositionS[i]
+            if (offsetPositionX[i] > 3f) offsetPositionX[i] = -3f
 
+            Matrix.setIdentityM(matM,0)
+            Matrix.translateM(matM,0,offsetPositionX[i],0.5f,offsetPositionZ[i])
+            Matrix.multiplyMM(matMVP,0,matVP,0,matM,0)
+            fogShader.draw(drawObjParticle,matM,matMVP,matTex,
+                    floatArrayOf(offsetTexCoordS[i],offsetTexCoordT[i]),u_depthCoef,0,1,u_softParticle)
+        }
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
@@ -195,22 +237,11 @@ class W061Renderer(ctx: Context): MgRenderer(ctx) {
         renderW = width
         renderH = height
 
-        // ノイズを生成するビットマップに描く
-        val noise = MyNoiseX(5,2,0.6f)
-        noise.seed = (SystemClock.uptimeMillis()/1000).toInt()
-        val size = 128
-        val noiseColor = FloatArray(size*size)
-        (0 until size).forEach { i ->
-            (0 until size).forEach { j ->
-                noiseColor[i*size+j] = noise.snoise(i.toFloat(),j.toFloat(),size.toFloat())
-            }
-        }
-        val bmp = noise.createImage(size,noiseColor)
 
         // テクスチャを作成
         GLES20.glGenTextures(1,textures,0)
         // テクスチャに使う画像をロード
-        MyGLFunc.createTexture(0,textures,bmp)
+        MyGLFunc.createTexture(0,textures,bmpArray[0])
 
         // フレームバッファ生成
         GLES20.glGenFramebuffers(1,bufFrame)
