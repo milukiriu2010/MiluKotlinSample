c324579716969434709d72395d2748f5da89aeba
milu(Mitsuharu Kiriu) <milu.kiriu2010@gmail.com>
2019-05-27 09:39:55 +0900

    w87

--- ExDB1/app/src/main/java/milu/kiriu2010/exdb1/opengl08/OpenGL08Activity.kt
+++ ExDB1/app/src/main/java/milu/kiriu2010/exdb1/opengl08/OpenGL08Activity.kt
@@ -7,6 +7,7 @@ import android.view.MenuItem
 import milu.kiriu2010.exdb1.R
 import milu.kiriu2010.exdb1.opengl08.w081.W081Fragment
 import milu.kiriu2010.exdb1.opengl08.w086.W086Fragment
+import milu.kiriu2010.exdb1.opengl08.w087.W087Fragment
 
 class OpenGL08Activity : AppCompatActivity() {
 
@@ -17,7 +18,7 @@ class OpenGL08Activity : AppCompatActivity() {
         supportFragmentManager.popBackStack()
         if (supportFragmentManager.findFragmentByTag("xyz") == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.frameLayout, W086Fragment.newInstance(), "xyz")
+                    .replace(R.id.frameLayout, W087Fragment.newInstance(), "xyz")
                     .commit()
         }
 
@@ -40,6 +41,16 @@ class OpenGL08Activity : AppCompatActivity() {
             // 前画面に戻る
             android.R.id.home -> {
                 finish()
+                true
+            }
+            // フラットシェーディング
+            R.id.opengl_w087 -> {
+                supportFragmentManager.popBackStack()
+                if (supportFragmentManager.findFragmentByTag("w087") == null) {
+                    supportFragmentManager.beginTransaction()
+                            .replace(R.id.frameLayout, W087Fragment.newInstance(), "w087")
+                            .commit()
+                }
                 true
             }
             // 描画結果から色を取得
---
+++ ExDB1/app/src/main/java/milu/kiriu2010/exdb1/opengl08/w087/W087Shader.kt
@@ -0,0 +1,121 @@
+package milu.kiriu2010.exdb1.opengl08.w087
+
+import android.opengl.GLES20
+import milu.kiriu2010.gui.model.MgModelAbs
+import milu.kiriu2010.gui.basic.MyGLFunc
+import milu.kiriu2010.gui.shader.MgShader
+
+// --------------------------------------
+// シェーダ(フラットシェーダ)
+// --------------------------------------
+// https://wgld.org/d/webgl/w087.html
+// --------------------------------------
+class W087Shader: MgShader() {
+    // 頂点シェーダ
+    private val scv =
+            """
+            attribute vec3  a_Position;
+            attribute vec4  a_Color;
+            uniform   mat4  u_matM;
+            uniform   mat4  u_matMVP;
+            varying   vec4  v_Position;
+            varying   vec4  v_Color;
+
+            void main() {
+                v_Color     = a_Color;
+                v_Position  = u_matM   * vec4(a_Position, 1.0);
+                gl_Position = u_matMVP * vec4(a_Position, 1.0);
+            }
+            """.trimIndent()
+
+    // フラグメントシェーダ
+    private val scf =
+            """
+            #extension GL_OES_standard_derivatives : enable
+
+            precision mediump   float;
+
+            uniform   vec3   u_vecLight;
+            varying   vec4   v_Position;
+            varying   vec4   v_Color;
+
+            void main() {
+                vec3 dx = dFdx(v_Position.xyz);
+                vec3 dy = dFdy(v_Position.xyz);
+                vec3 n  = normalize(cross(normalize(dx),normalize(dy)));
+
+                vec3  light  = normalize(u_vecLight);
+                float diff   = clamp(dot(n,light),0.1,1.0);
+                gl_FragColor = vec4(v_Color.rgb * diff, 1.0);
+            }
+            """.trimIndent()
+
+    override fun loadShader(): MgShader {
+        // 頂点シェーダを生成
+        svhandle = MyGLFunc.loadShader(GLES20.GL_VERTEX_SHADER, scv)
+        // フラグメントシェーダを生成
+        sfhandle = MyGLFunc.loadShader(GLES20.GL_FRAGMENT_SHADER, scf)
+
+        // プログラムオブジェクトの生成とリンク
+        programHandle = MyGLFunc.createProgram(svhandle,sfhandle, arrayOf("a_Position","a_Color") )
+        return this
+    }
+
+    fun draw(model: MgModelAbs,
+             matM: FloatArray,
+             matMVP: FloatArray,
+             u_vecLight: FloatArray) {
+
+        GLES20.glUseProgram(programHandle)
+        MyGLFunc.checkGlError2("UseProgram",this,model)
+
+        // attribute(頂点)
+        model.bufPos.position(0)
+        val hPosition = GLES20.glGetAttribLocation(programHandle,"a_Position").also {
+            if ( it != -1 ) {
+                GLES20.glVertexAttribPointer(it,3,GLES20.GL_FLOAT,false, 3*4, model.bufPos)
+                GLES20.glEnableVertexAttribArray(it)
+            }
+        }
+        MyGLFunc.checkGlError2("a_Position",this,model)
+
+        // attribute(色)
+        model.bufCol.position(0)
+        val hColor = GLES20.glGetAttribLocation(programHandle,"a_Color").also {
+            if ( it != -1 ) {
+                GLES20.glVertexAttribPointer(it,4,GLES20.GL_FLOAT,false, 4*4, model.bufCol)
+                GLES20.glEnableVertexAttribArray(it)
+            }
+        }
+        MyGLFunc.checkGlError2("a_Color",this,model)
+
+        // uniform(モデル座標変換行列)
+        GLES20.glGetUniformLocation(programHandle,"u_matM").also {
+            GLES20.glUniformMatrix4fv(it,1,false,matM,0)
+        }
+        MyGLFunc.checkGlError2("u_matM",this,model)
+
+        // uniform(モデル×ビュー×プロジェクション)
+        GLES20.glGetUniformLocation(programHandle,"u_matMVP").also {
+            GLES20.glUniformMatrix4fv(it,1,false,matMVP,0)
+        }
+        MyGLFunc.checkGlError2("u_matMVP",this,model)
+
+        // uniform(光源位置)
+        GLES20.glGetUniformLocation(programHandle,"u_vecLight").also {
+            GLES20.glUniform3fv(it,1,u_vecLight,0)
+        }
+        MyGLFunc.checkGlError2("u_vecLight",this,model)
+
+        // モデルを描画
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, model.datIdx.size, GLES20.GL_UNSIGNED_SHORT, model.bufIdx)
+
+        // リソース解放
+        if ( hPosition != -1 ) {
+            GLES20.glDisableVertexAttribArray(hPosition)
+        }
+        if ( hColor != -1 ) {
+            GLES20.glDisableVertexAttribArray(hColor)
+        }
+    }
+}
